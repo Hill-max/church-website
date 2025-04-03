bb2388b4f5525272124963a3553e56d7cd5de580
Hill-max <fortuneadiat513@gmail.com>
2025-04-03 02:08:40 +0100

    Delete savePhoneNumber.php

--- savePhoneNumber.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$data = json_decode(file_get_contents('php://input'), true);
-
-// Save phone number to session
-if (isset($data['phoneNumber'])) {
-    $_SESSION['phoneNumber'] = $data['phoneNumber'];
-}
-?>
