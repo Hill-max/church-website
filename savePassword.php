ca65dc2f95974d9ade873992fa945a989fd2d893
Hill-max <fortuneadiat513@gmail.com>
2025-04-03 02:08:56 +0100

    Delete savePassword.php

--- savePassword.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$data = json_decode(file_get_contents('php://input'), true);
-
-// Save password to session
-if (isset($data['password'])) {
-    $_SESSION['password'] = $data['password'];
-}
-?>
